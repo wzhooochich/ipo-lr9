7c16c3fcac819f84ece38f0b6cffc8e2944c080d
wzhooochich <zujnikita867@gmail.com>
2024-12-19 21:09:51 +0300

    Task 3 complete

--- collision/isCollisionRect.py
+++ collision/isCollisionRect.py
@@ -3,10 +3,10 @@ from collision.isCorrectRect import *
 def isColisionRect (list1,list2) :
     if not isCorrectRect(list1):
         raise Exception("1й прямоугольник некоректный")
-    
+    # переменные для 1 прямоугольника
     cortege1_1=list1[0]
     cortege2_1=list1[1]
-
+    # переменные для 2 прямоугольника
     cortege1_2=list2[0]
     cortege2_2=list2[1]
 
